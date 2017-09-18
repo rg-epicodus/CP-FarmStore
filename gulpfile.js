3a59a750115127863b365d9762eb412fcc97bd31
Rich Garrick <richg341@gmail.com>
2017-09-18 10:01:11 -0700

    add css styling, content

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
-  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch([ ['js/*.js'], ['index.html'], ['css/styles.css'] ], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
 });
 
